4b182fa1ad4b595145dc7d634427ec1317c70e9c
dustinkiselbach <52965183+dustinkiselbach@users.noreply.github.com>
2020-07-13 14:14:04 -0700

    fixed axios interceptor

--- src/utils/setAuthToken.ts
+++ src/utils/setAuthToken.ts
@@ -1,9 +1,10 @@
-import axios from 'axios'
+import axios, { AxiosRequestConfig } from 'axios'
+import moment from 'moment'
+import qs from 'qs'
 
 export const setAuthToken = (token?: string) => {
   if (token) {
     // apply to every request
-    console.log(token)
 
     axios.defaults.headers.common['Authorization'] = token
   } else {
@@ -21,7 +22,10 @@ axios.interceptors.request.use(
       if (new Date(exp) < now) {
         console.log('expired')
         localStorage.clear()
-        window.location.reload()
+        return applicationOnlyAuth().then((token: string) => {
+          setAuthToken(token)
+          return Promise.resolve(config)
+        })
       }
     }
     return config
@@ -30,3 +34,36 @@ axios.interceptors.request.use(
     return Promise.reject(err)
   }
 )
+
+const applicationOnlyAuth = async () => {
+  const body = {
+    grant_type: 'client_credentials'
+  }
+
+  const config: AxiosRequestConfig = {
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    auth: {
+      username: process.env.REACT_APP_CLIENT_ID!,
+      password: process.env.REACT_APP_SECRET_KEY!
+    }
+  }
+
+  try {
+    const res = await axios.post(
+      'https://www.reddit.com/api/v1/access_token',
+      qs.stringify(body),
+      config
+    )
+
+    const exp = moment().add(res.data.expires_in, 's')
+
+    localStorage.setItem('token', res.data.access_token)
+    localStorage.setItem('exp', exp.toString())
+
+    return res.data.access_token
+  } catch (err) {
+    throw err
+  }
+}
